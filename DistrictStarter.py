95d2bd40595e293318646c7dd23c03c30082a7be
Rocket Programmer <rocketgamer580@gmail.com>
2020-04-30 23:39:13 -0700

    idk: Fun shiz

---
+++ DistrictStarter.py
@@ -0,0 +1,43 @@
+import os, subprocess
+
+districtNames = [
+    'Boingy Acres'
+    'Boingy Bay',
+    'Boingy Summit',
+    'Boingyboro',
+    'Bouncy Summit'
+    'Boingyboro',
+    'Bouncy Summit',
+    'Crazy Grove',
+    'Crazy Hills',
+    'Crazyham',
+    'Funnyfield',
+    'Giggly Bay',
+    'Giggly Grove',
+    'Giggly Hills',
+    'Giggly Point',
+    'Gigglyfield',
+    'Gigglyham',
+    'Goofy Valley',
+    'Goofyport',
+    'Kooky Grove',
+    'Kookyboro',
+    'Loopy Harbor',
+    'Nutty Hills',
+    'Nutty River',
+    'Nutty Summit',
+    'Nuttyville',
+    'Nuttywood',
+    'Silly Rapids',
+    'Silly Valley',
+    'Sillyham',
+    'Toon Valley',
+    'Zany Acres'
+]
+
+startingNum = 401000000
+
+for index, elem in enumerate(districtNames):
+    subprocess.shell = True
+    os.system('start cmd /c districtStarter.bat ' + str(districtNames[index]) + ' ' + str(startingNum))
+    startingNum = startingNum + 1000000
---
+++ districtStarter.bat
@@ -0,0 +1,10 @@
+set DISTRICT_NAME=%~1 %~2
+
+set BASE_CHANNEL=%~3
+
+title %DISTRICT_NAME%
+
+:main
+C:/Panda3D-1.11.0-x64-astron/python/python -m game.toontown.ai.AIStart config/Config.prc
+pause
+goto :main
