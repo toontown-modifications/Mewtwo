5037b17f44025d3cd147e98f85bd572f3331068c
Rocket Programmer <rocketgamer580@gmail.com>
2020-05-20 17:16:30 -0700

    magic-words: Better var name

--- game/toontown/ai/ToontownMagicWordManagerAI.py
+++ game/toontown/ai/ToontownMagicWordManagerAI.py
@@ -35,7 +35,7 @@ class ToontownMagicWordManagerAI(MagicWordManagerAI):
         self.air = air
 
         self.wantSystemResponses = config.GetBool('want-system-responses', False)
-        self.overrideViaExt = False
+        self.overrideResponseType = False
 
     def generate(self):
         MagicWordManagerAI.generate(self)
@@ -416,7 +416,7 @@ class ToontownMagicWordManagerAI(MagicWordManagerAI):
         if not message or not invoker:
             return
 
-        if self.overrideViaExt or self.wantSystemResponses:
+        if self.overrideResponseType or self.wantSystemResponses:
             invoker.d_setSystemMessage(0, message)
         else:
             self.sendUpdateToAvatarId(invokerId, 'setMagicWordResponse', [message])
@@ -783,7 +783,7 @@ class ToontownMagicWordManagerAI(MagicWordManagerAI):
             avId = self.air.getAvatarIdFromSender()
 
         if sentFromExt:
-            self.overrideViaExt = True
+            self.overrideResponseType = True
 
         av = self.air.doId2do.get(avId)
 
