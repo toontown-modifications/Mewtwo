fca94bb1284d73d392995a8662f47a4b9bb5ef32
Rocket Programmer <rocketgamer580@gmail.com>
2020-06-20 14:04:51 -0700

    Revert "AI: Start working on sendQueryToonMaxHp/SecurityMgr"
    This reverts commit 5f768eedba805e0d569c2abdeedce1bd31deae1b.

--- game/toontown/uberdog/DistributedSecurityMgrAI.py
+++ game/toontown/uberdog/DistributedSecurityMgrAI.py
@@ -1,16 +1,13 @@
-from direct.directnotify.DirectNotifyGlobal import directNotify
+from direct.directnotify import DirectNotifyGlobal
 from direct.distributed.DistributedObjectGlobalAI import DistributedObjectGlobalAI
 
+
 class DistributedSecurityMgrAI(DistributedObjectGlobalAI):
-    notify = directNotify.newCategory('DistributedSecurityMgrAI')
+    notify = DirectNotifyGlobal.directNotify.newCategory(
+        "DistributedSecurityMgrAI")
 
-    def requestAccountId(self, avId, callback):
-        if self.notify.getDebug():
-            self.notify.debug('Got avId: {0} and callback {1}.'.format(avId, callback))
-
-        accId = self.air.GetAccountIDFromChannelCode(avId)
-
-        return accId
+    def requestAccountId(self, todo0, todo1, todo2):
+        pass
 
     def requestAccountIdResponse(self, todo0, todo1):
         pass
