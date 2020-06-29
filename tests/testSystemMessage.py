26217f92bdc9ee0b78779296748203875913b39a
Rocket Programmer <rocketgamer580@gmail.com>
2020-06-28 19:58:29 -0700

    tests: Rewrite

--- tests/testSystemMessage.py
+++ tests/testSystemMessage.py
@@ -1,30 +1,15 @@
-from twisted.internet import reactor
-from twisted.internet.protocol import Protocol, ClientFactory
+import socket, base64, json
 
-import base64, json
+messageRequest = {
+    'whatToDo': 'systemMessage',
+    'message': 'System message test from Twisted server.',
+    'signature': base64.b64decode('UGxlYXNlRG9Ob3RBYnVzZVRoaXNGdW5jdGlvbg==')
+}
 
-class Main(Protocol):
-    def connectionMade(self):
-        kickRequest = {
-            'whatToDo': 'systemMessage',
-            'message': 'System message test from Twisted server.',
-            'signature': base64.b64decode('UGxlYXNlRG9Ob3RBYnVzZVRoaXNGdW5jdGlvbg==')
-        }
+request = json.dumps(messageRequest)
 
-        request = json.dumps(kickRequest)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 7199))
 
-        self.transport.write(request)
-
-class MainFactory(ClientFactory):
-    def buildProtocol(self, addr):
-        print('Connected!')
-        return Main()
-
-    def clientConnectionLost(self, connector, reason):
-        print('Connection lost!')
-
-    def clientConnectionFailed(self, connector, reason):
-        print('Connection failed!')
-
-reactor.connectTCP('127.0.0.1', 7199, MainFactory())
-reactor.run()
+# Send our request.
+client.sendall(request)
