a04f3c4cdc08340f237b41b74750b9262828628d
Rocket Programmer <rocketgamer580@gmail.com>
2020-07-02 16:08:29 -0400

    linux: Fix

--- startup/linux/run_ai.sh
+++ startup/linux/run_ai.sh
@@ -3,8 +3,8 @@ cd ../..
 
 # Get the user input:
 read -p "District name (DEFAULT: Sillyville): " DISTRICT_NAME
-DISTRICT_NAME=${DISTRICT_NAME:-Sillyville}
+export DISTRICT_NAME=${DISTRICT_NAME:-Sillyville}
 read -p "Base channel (DEFAULT: 401000000): " BASE_CHANNEL
-BASE_CHANNEL=${BASE_CHANNEL:-401000000}
+export BASE_CHANNEL=${BASE_CHANNEL:-401000000}
 
 python -m game.toontown.ai.AIStart config/Config.prc
