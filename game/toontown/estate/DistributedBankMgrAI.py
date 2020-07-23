66ae2d5cc24dcbcd26074239cfcb962bedd25b3c
Rocket Programmer <rocketgamer580@gmail.com>
2020-07-22 23:58:57 -0700

    bank-mgr: Implement missing transferMoney

--- game/toontown/estate/DistributedBankMgrAI.py
+++ game/toontown/estate/DistributedBankMgrAI.py
@@ -5,4 +5,36 @@ class DistributedBankMgrAI(DistributedObjectAI):
     notify = directNotify.newCategory('DistributedBankMgrAI')
 
     def transferMoney(self, amount):
-        pass
+        avId = self.air.getAvatarIdFromSender()
+
+        if not avId:
+            return
+
+        av = self.air.doId2do.get(avId)
+
+        if not av:
+            return
+
+        transactionAmount = amount
+
+        jarMoney = av.getMoney()
+        maxJarMoney = av.getMaxMoney()
+        bankMoney = av.getBankMoney()
+        maxBankMoney = av.getMaxBankMoney()
+
+        transactionAmount = min(transactionAmount, jarMoney)
+        transactionAmount = min(transactionAmount, maxBankMoney - bankMoney)
+        transactionAmount = -min(-transactionAmount, maxJarMoney - jarMoney)
+        transactionAmount = -min(-transactionAmount, bankMoney)
+
+        newJarMoney = jarMoney - transactionAmount
+        newBankMoney = bankMoney + transactionAmount
+
+        if newJarMoney > maxJarMoney:
+            return
+
+        if newBankMoney > maxBankMoney:
+            return
+
+        av.b_setMoney(newJarMoney)
+        av.b_setBankMoney(newBankMoney)
