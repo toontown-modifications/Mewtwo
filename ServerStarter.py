4c877dfc2bcd802cc0da56032178c89c4234d957
Rocket Programmer <rocketgamer580@gmail.com>
2020-09-28 11:32:51 -0700

    Add EndpointManager to start_all

--- ServerStarter.py
+++ ServerStarter.py
@@ -1,4 +1,4 @@
-# Starts up the OTP/External/AI server with screen all in one go.
+# Starts up the OTP/External/AI/Endpoints with screen all in one go.
 # Currently only supports Unix based systems.
 
 import os
@@ -8,3 +8,4 @@ os.chdir('startup/unix')
 os.system('screen -dmS OTP ./run_otpd.sh')
 os.system('screen -dmS External ./run_server_ext.sh')
 os.system('screen -dmS AI ./run_ai.sh')
+os.system('screen -dmS Endpoints ./run_endpoint_manager.sh')
