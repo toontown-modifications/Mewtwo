755f45ca7de54dd8ec34f249e0dc6e0b5b111ce1
Rocket Programmer <rocketgamer580@gmail.com>
2020-10-02 20:08:08 -0700

    hm

--- ServerStarter.py
+++ ServerStarter.py
@@ -1,12 +1,20 @@
 # Starts up all the services using screen all in one go.
 # Currently only supports Unix based systems.
 
-import os
+import sys, os
+
+isProduction = '--prod' in sys.argv
 
 os.chdir('startup/unix')
 
-os.system('screen -dmS OTP ./run_otpd.sh')
+if sys.platform == 'linux2':
+    os.system('screen -dmS OTP ./run_otpd.sh')
+else:
+    os.system('screen -dms OTP ./run_otpd_darwin.sh')
+
 os.system('screen -dmS External ./run_server_ext.sh')
 os.system('screen -dmS AI ./run_ai.sh')
-os.system('screen -dmS Stunnel ./run_stunnel.sh')
-os.system('screen -dmS Endpoints ./run_endpoint_manager.sh')
+
+if isProduction:
+    os.system('screen -dmS Stunnel ./run_stunnel.sh')
+    os.system('screen -dmS Endpoints ./run_endpoint_manager.sh')
