8a1365befbb991e41ca093e078d531163107fa80
Rocket Programmer <rocketgamer580@gmail.com>
2020-10-24 15:50:41 -0700

    ext: Try this

--- game/toontown/uberdog/ExtAgent.py
+++ game/toontown/uberdog/ExtAgent.py
@@ -1314,7 +1314,7 @@ class ExtAgent(ServerBase):
 
                 loaderName = ZoneUtil.getLoaderName(zoneId)
 
-                if branchId and isPlayground or isCogHQ and loaderName != 'safeZoneLoader':
+                if branchId and zoneId != branchId or zoneId >= 10000 and zoneId != 11200 and not isPlayground:
                     # Set object location.
                     dg = PyDatagram()
                     dg.addServerHeader(clientChannel, self.air.ourChannel, CLIENT_OBJECT_LOCATION)
