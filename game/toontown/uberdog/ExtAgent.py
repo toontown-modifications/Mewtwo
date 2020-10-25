8c8084d4ff69ee6434423487b08544c9aff7ad06
Rocket Programmer <rocketgamer580@gmail.com>
2020-10-24 17:14:17 -0700

    ext: Revert back to previous check

--- game/toontown/uberdog/ExtAgent.py
+++ game/toontown/uberdog/ExtAgent.py
@@ -1314,7 +1314,7 @@ class ExtAgent(ServerBase):
 
                 loaderName = ZoneUtil.getLoaderName(zoneId)
 
-                if branchId and isPlayground or isCogHQ and loaderName != 'safeZoneLoader':
+                if branchId and zoneId != branchId and isPlayground or isCogHQ and loaderName != 'safeZoneLoader':
                     # Set object location.
                     dg = PyDatagram()
                     dg.addServerHeader(clientChannel, self.air.ourChannel, CLIENT_OBJECT_LOCATION)
