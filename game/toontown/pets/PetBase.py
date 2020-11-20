c7e0acae6178deb6729814cb317251bf64dce565
Rocket Programmer <rocketgamer580@gmail.com>
2020-11-20 10:35:08 -0800

    pets: Fix PetBase for Python 3

--- game/toontown/pets/PetBase.py
+++ game/toontown/pets/PetBase.py
@@ -1,11 +1,10 @@
 from game.toontown.pets.PetConstants import AnimMoods
 from game.toontown.pets import PetMood
-import string
 
 class PetBase:
 
     def getSetterName(self, valueName, prefix = 'set'):
-        return '%s%s%s' % (prefix, string.upper(valueName[0]), valueName[1:])
+        return '%s%s%s' % (prefix, valueName[0].upper(), valueName[1:])
 
     def getAnimMood(self):
         if self.mood.getDominantMood() in PetMood.PetMood.ExcitedMoods:
