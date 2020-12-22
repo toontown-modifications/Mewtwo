b0e92efeafc4cdd18cfdb7e0b5faee6e526b1c45
Rocket Programmer <rocketgamer580@gmail.com>
2020-12-22 14:50:08 -0800

    building: Potentially fix AI startup

--- game/toontown/building/DistributedHQInteriorAI.py
+++ game/toontown/building/DistributedHQInteriorAI.py
@@ -35,10 +35,10 @@ class DistributedHQInteriorAI(DistributedObjectAI.DistributedObjectAI):
     def sendNewLeaderBoard(self):
         if self.air:
             self.isDirty = False
-            self.sendUpdate('setLeaderBoard', [pickle.dumps(list(self.air.trophyMgr.getLeaderInfo()), 1)])
+            self.sendUpdate('setLeaderBoard', [pickle.dumps(self.air.trophyMgr.getLeaderInfo(), 1)])
 
     def getLeaderBoard(self):
-        return pickle.dumps(self.air.trophyMgr.getLeaderInfo(), 1)
+        return pickle.dumps(list(self.air.trophyMgr.getLeaderInfo()), 1)
 
     def getTutorial(self):
         return self.tutorial
