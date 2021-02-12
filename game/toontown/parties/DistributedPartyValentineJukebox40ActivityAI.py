533141cd57d3e23823eb6580703ed9400cc40770
Rocket Programmer <rocketgamer580@gmail.com>
2021-02-11 18:18:34 -0800

    parties: PartyGlobals

--- game/toontown/parties/DistributedPartyValentineJukebox40ActivityAI.py
+++ game/toontown/parties/DistributedPartyValentineJukebox40ActivityAI.py
@@ -1,5 +1,6 @@
 from direct.directnotify.DirectNotifyGlobal import directNotify
 from game.toontown.parties.DistributedPartyJukeboxActivityBaseAI import DistributedPartyJukeboxActivityBaseAI
+from game.toontown.parties import PartyGlobals
 
 class DistributedPartyValentineJukebox40ActivityAI(DistributedPartyJukeboxActivityBaseAI):
     notify = directNotify.newCategory('DistributedPartyValentineJukebox40ActivityAI')
