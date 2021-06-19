2ee8d5ac15ea4907ddd516372aba70058d485696
Rocket Programmer <rocketgamer580@gmail.com>
2021-06-18 23:29:38 -0700

    Revert "toon: RDB"
    This reverts commit 89b5fed2197474551e19bcc8556d62a86e0cdd0b.

--- game/toontown/toon/DistributedToonAI.py
+++ game/toontown/toon/DistributedToonAI.py
@@ -3198,7 +3198,7 @@ class DistributedToonAI(DistributedPlayerAI.DistributedPlayerAI, DistributedSmoo
 
         def _moveSphere(self, task):
             self.collNodePath.setZ(self.getRender(), 0)
-            yield
+            return Task.cont
 
         def isInEstate(self):
             return hasattr(self, 'inEstate') and self.inEstate
