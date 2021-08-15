bb4dcbe8bbc63327d59181b797475770828f932a
Rocket Programmer <rocketgamer580@gmail.com>
2021-08-15 12:09:01 -0500

    battle: XP

--- game/toontown/battle/DistributedBattleBaseAI.py
+++ game/toontown/battle/DistributedBattleBaseAI.py
@@ -50,12 +50,12 @@ class DistributedBattleBaseAI(DistributedObjectAI.DistributedObjectAI, BattleBas
         self.toonMerits = {}
         self.toonParts = {}
         self.battleCalc = BattleCalculatorAI.BattleCalculatorAI(self, tutorialFlag)
+        mult = 1.0
         if self.air.suitInvasionManager.getInvading():
-            mult = getInvasionMultiplier()
-            self.battleCalc.setSkillCreditMultiplier(mult)
+            mult *= getInvasionMultiplier()
         if self.air.holidayManager.isMoreXpHolidayRunning():
-            mult = getMoreXpHolidayMultiplier()
-            self.battleCalc.setSkillCreditMultiplier(mult)
+            mult *= getMoreXpHolidayMultiplier()
+        self.battleCalc.setSkillCreditMultiplier(mult)
         self.fsm = None
         self.clearAttacks()
         self.ignoreFaceOffDone = 0
