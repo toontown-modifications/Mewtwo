0db064d573ab92d897d67ebbf695049c875acd65
Rocket Programmer <rocketgamer580@gmail.com>
2021-09-06 14:10:26 -0700

    safe-zone-mgr: Disney accuracy

--- game/toontown/safezone/SafeZoneManagerAI.py
+++ game/toontown/safezone/SafeZoneManagerAI.py
@@ -1,21 +1,31 @@
-from direct.directnotify.DirectNotifyGlobal import *
-from direct.distributed import DistributedObjectAI
+from direct.directnotify.DirectNotifyGlobal import directNotify
+from direct.distributed.DistributedObjectAI import DistributedObjectAI
 
-HealFrequency = 10.0  # The time in seconds between each Toon-up pulse.
+HealFrequency = 10.0 # The time in seconds between each Toon-up pulse.
 
-class SafeZoneManagerAI(DistributedObjectAI.DistributedObjectAI):
+class SafeZoneManagerAI(DistributedObjectAI):
     notify = directNotify.newCategory('SafeZoneManagerAI')
 
     def enterSafeZone(self):
         avId = self.air.getAvatarIdFromSender()
         av = self.air.doId2do.get(avId)
+
         if not av:
             return
+
+        # Start healing this avatar.
         av.startToonUp(HealFrequency)
+
+        # Used for avatars that are in battle in case they escape.
+        event = 'inSafezone-{0}'.format(avId)
+        messenger.send(event)
 
     def exitSafeZone(self):
         avId = self.air.getAvatarIdFromSender()
         av = self.air.doId2do.get(avId)
+
         if not av:
             return
+
+        # Stop healing this avatar.
         av.stopToonUp()
