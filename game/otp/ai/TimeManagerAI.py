48e86d57fe8a5c4a30c872da0e0f8b85f8f22b0b
Rocket Programmer <rocketgamer580@gmail.com>
2022-03-11 16:21:24 -0800

    time-manager: Use Anesidora AI

--- game/otp/ai/TimeManagerAI.py
+++ game/otp/ai/TimeManagerAI.py
@@ -1,54 +1,69 @@
-from panda3d.core import HashVal
-from direct.distributed.DistributedObjectAI import DistributedObjectAI
-from direct.directnotify.DirectNotifyGlobal import directNotify
-from direct.distributed.ClockDelta import globalClockDelta
-
+from .AIBaseGlobal import *
+from pandac.PandaModules import *
+from direct.distributed.ClockDelta import *
+from direct.task import Task
+from direct.distributed import DistributedObjectAI
+from direct.directnotify import DirectNotifyGlobal
+from direct.showbase import GarbageReport
 from game.otp.otpbase import OTPGlobals
-
+from game.otp.ai.GarbageLeakServerEventAggregatorAI import GarbageLeakServerEventAggregatorAI
 import time
 
-class TimeManagerAI(DistributedObjectAI):
-    notify = directNotify.newCategory('TimeManagerAI')
+class TimeManagerAI(DistributedObjectAI.DistributedObjectAI):
+    notify = DirectNotifyGlobal.directNotify.newCategory("TimeManagerAI")
 
     def __init__(self, air):
-        DistributedObjectAI.__init__(self, air)
-
-        # Dictionaries:
-        self.avId2disconnectcode = {}
-        self.avId2exceptioninfo = {}
-        self.avId2CpuInfo = {}
-        self.avId2cacheStatus = {}
+        DistributedObjectAI.DistributedObjectAI.__init__(self, air)
+        if not __dev__:
+            # double-check that we're not implementing a client-sendable debug DC method in production
+            if hasattr(self, 'checkForGarbageLeaks'):
+                self.notify.error('checkForGarbageLeaks should not be defined outside of __dev__')
 
     def requestServerTime(self, context):
-        avId = self.air.getAvatarIdFromSender()
+        """requestServerTime(self, int8 context)
 
-        if not avId:
-            return
-
-        networkTime = globalClockDelta.getRealNetworkTime(bits = 32)
-        currentTime = int(time.time())
-
-        self.sendUpdateToAvatarId(avId, 'serverTime', [context, networkTime, currentTime])
-
+        This message is sent from the client to the AI to initiate a
+        synchronization phase.  The AI should immediately report back
+        with its current time.  The client will then measure the round
+        trip.
+        """
+        timestamp = globalClockDelta.getRealNetworkTime(bits=32)
+        requesterId = self.air.getAvatarIdFromSender()
+        timeOfDay = int(time.time())
+        self.sendUpdateToAvatarId(requesterId, "serverTime",
+                                  [context, timestamp, timeOfDay])
+        
     def setDisconnectReason(self, disconnectCode):
-        avId = self.air.getAvatarIdFromSender()
+        """setDisconnectReason(self, uint8 disconnectCode)
 
-        if not avId:
-            return
+        This method is called by the client just before it leaves a
+        shard to alert the AI as to the reason it's going.  If the AI
+        doesn't get this message, it can assume the client aborted
+        messily or its internet connection was dropped.
+        """
+        requesterId = self.air.getAvatarIdFromSender()
+        self.notify.info("Client %s leaving for reason %s (%s)." % (
+            requesterId, disconnectCode,
+            OTPGlobals.DisconnectReasons.get(disconnectCode,
+                                             'invalid reason')))
 
-        reason = OTPGlobals.DisconnectReasons.get(disconnectCode, 'unknown')
-
-        self.avId2disconnectcode[avId] = disconnectCode
-        self.air.writeServerEvent('disconnect-reason', avId = avId, reason = reason)
-
+        if disconnectCode in OTPGlobals.DisconnectReasons:
+            self.air.setAvatarDisconnectReason(requesterId, disconnectCode)
+        else:
+            self.air.writeServerEvent(
+                'suspicious', requesterId, 'invalid disconnect reason: %s' % disconnectCode)
+        
     def setExceptionInfo(self, info):
-        avId = self.air.getAvatarIdFromSender()
+        """setExceptionInfo(self, string info)
 
-        if not avId:
-            return
-
-        self.avId2exceptioninfo[avId] = info
-        self.air.writeServerEvent('client-exception', avId = avId, info = info)
+        In the case of the client leaving for a Python exception, we
+        also follow up the above message with this one, which just
+        sends a text string describing the exception for the AI log.
+        """
+        requesterId = self.air.getAvatarIdFromSender()
+        self.notify.info("Client %s exception: %s" % (requesterId, info))
+        serverVersion = simbase.config.GetString('server-version','')
+        self.air.writeServerEvent('client-exception', requesterId, '%s|%s' % (serverVersion,info))
 
     def setSignature(self, signature, hash, pyc):
         """
@@ -71,28 +86,69 @@ class TimeManagerAI(DistributedObjectAI):
             self.notify.info('Client %s py signature: %s' % (requesterId, info))
             self.air.writeServerEvent('client-py-signature', requesterId, info)
 
-    def setFrameRate(self, todo0, todo1, todo2, todo3, todo4, todo5, todo6,
-                     todo7, todo8, todo9, todo10, todo11, todo12, todo13,
-                     todo14, todo15, todo16, todo17):
-        pass
-
     def setCpuInfo(self, info, cacheStatus):
-        avId = self.air.getAvatarIdFromSender()
+        """
+        This method is called by the client at startup time, to send
+        the detailed CPU information to the server for logging.
+        """
+        requesterId = self.air.getAvatarIdFromSender()
+        
+        self.notify.info('client-cpu %s|%s' % (requesterId, info))
+        self.air.writeServerEvent('client-cpu', requesterId, info)
+        # We call this cacheStatus, but really it's the mac address or
+        # other client fingerprint information, in a simple
+        # obfuscating cipher.  Decode it.
+        key = 'outrageous'
+        p = 0
+        fingerprint = ''
+        for ch in cacheStatus:
+            ic = ord(ch) ^ ord(key[p])
+            p += 1
+            if p >= len(key):
+                p = 0
+            fingerprint += chr(ic)
 
-        if not avId:
-            return
+        self.notify.info('client-fingerprint %s|%s' % (requesterId, fingerprint))
+        self.air.writeServerEvent('client-fingerprint', requesterId, fingerprint)
+        if hasattr(self.air, 'cpuInfoMgr'):
+            self.air.cpuInfoMgr.sendCpuInfoToUd(info, fingerprint)
 
-        self.avId2CpuInfo[avId] = info
-        self.avId2cacheStatus[avId] = cacheStatus
 
-    def checkForGarbageLeaks(self, todo0):
-        pass
+    def setFrameRate(self, fps, deviation, numAvs,
+                     locationCode, timeInLocation, timeInGame,
+                     gameOptionsCode, vendorId, deviceId,
+                     processMemory, pageFileUsage, physicalMemory,
+                     pageFaultCount, osInfo, cpuSpeed,
+                     numCpuCores, numLogicalCpus, apiName):
+        """ This method is called by the client at the interval
+        specified by getFrameRateInterval(), to report its current
+        frame rate. """
 
-    def setNumAIGarbageLeaks(self, todo0):
-        pass
+        requesterId = self.air.getAvatarIdFromSender()
+        info = '%0.1f fps|%0.3fd|%s avs|%s|%d|%d|%s|0x%04x|0x%04x|%0.1fMB|%0.1fMB|%0.1fMB|%d|%s|%s|%s cpus|%s' % (
+            fps, deviation, numAvs, locationCode, timeInLocation,
+            timeInGame, gameOptionsCode,
+            vendorId, deviceId, processMemory, pageFileUsage, physicalMemory,
+            pageFaultCount, '%s.%d.%d.%d' % osInfo, '%0.03f,%0.03f' % cpuSpeed,
+            '%d,%d' % (numCpuCores, numLogicalCpus),
+            apiName)
+        self.notify.info('client-fps %s|%s' % (requesterId, info))
+        self.air.writeServerEvent('client-fps', requesterId, info)
 
-    def setClientGarbageLeak(self, todo0, todo1):
-        pass
+    if __dev__:
+        def checkForGarbageLeaks(self, wantReply):
+            senderId = self.air.getAvatarIdFromSender()
+            self.notify.info("checking for garbage leaks requested by %s" % senderId)
+            # okay checking for garbage leaks should only be done by devs, it's rare enough i'll flag it
+            # as suspicious
+            self.air.writeServerEvent('suspicious', senderId, 'checkForGarbageLeaks')
+            numLeaks = GarbageReport.checkForGarbageLeaks()
+            if wantReply:
+                requesterId = self.air.getAvatarIdFromSender()
+                self.sendUpdateToAvatarId(requesterId, 'setNumAIGarbageLeaks', [numLeaks])
+
+        def setClientGarbageLeak(self, num, description):
+            messenger.send(GarbageLeakServerEventAggregatorAI.ClientLeakEvent, [num, description])
 
     def checkAvOnDistrict(self, context, avId):
         av = self.air.doId2do.get(avId)
@@ -102,4 +158,5 @@ class TimeManagerAI(DistributedObjectAI):
         else:
             isOnShard = False
 
-        #self.sendUpdateToAvatarId(avId, 'checkAvOnDistrictResult', [context, avId, isOnShard])
+        print(f'checkAvOnDistrictResult: {context} - {avId} - {isOnShard}')
+        self.sendUpdateToAvatarId(avId, 'checkAvOnDistrictResult', [context, avId, isOnShard])
