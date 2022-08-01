7f0a3bd79c8f81af929562d8efe8daa5b5f348c4
Rocket <me@rocketprogrammer.me>
2022-08-01 17:39:38 -0500

    game: Try this

--- ServerStarter.py
+++ ServerStarter.py
@@ -17,7 +17,8 @@ os.system('screen -dmS External ./run_server_ext.sh')
 # Wait until the external server starts.
 time.sleep(15)
 
-os.system('cd ../.. && screen -dmS Districts python3 -m DistrictStarter')
+# os.system('cd ../.. && screen -dmS Districts python3 -m DistrictStarter')
+os.system('screen -dmS Sillyville ./run_first_ai.sh')
 
 if isProduction:
     os.system('screen -dmS Stunnel ./run_stunnel.sh')
