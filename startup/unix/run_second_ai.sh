86945e271229cf6108f0994b98596a1b1dcf6cbb
Rocket <me@rocketprogrammer.me>
2022-08-11 10:53:19 -0500

    unix: Better

--- startup/unix/run_second_ai.sh
+++ startup/unix/run_second_ai.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd ../..
 
-export DISTRICT_NAME="Nutty River"
-export BASE_CHANNEL=403000000
+export DISTRICT_NAME="Sillyham"
+export BASE_CHANNEL=402000000
 
 screen -dmS "$DISTRICT_NAME" python3 -m game.toontown.ai.AIStart config/Config.prc
